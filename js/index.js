1763814788d4043f9275134261e36ff6bb28d7b2
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-22 19:32:08 -0300

    Object Orientation in JavaScript

--- js/index.js
+++ js/index.js
@@ -1,32 +1,14 @@
-const routes = {
-  "/": "/pages/home.html",
-  "/about": "/pages/about.html",
-  "/contact": "/pages/contact.html",
-  404: "/pages/404.html",
-}
+import "./router.js"
 
-function route(event) {
-  event = event || window.event
-  event.preventDefault()
+// const routes = {
+//   "/": "/pages/home.html",
+//   "/about": "/pages/about.html",
+//   "/contact": "/pages/contact.html",
+//   404: "/pages/404.html",
+// }
 
-  window.history.pushState({}, "", event.target.href)
-
-  handle()
-}
-
-function handle() {
-  // desestruturando, ou seja "tirando" de dentro do objeto e passando para a const
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-
-  fetch(route)
-    .then((data) => data.text())
-    .then((html) => {
-      document.querySelector("#app").innerHTML = html
-    })
-}
-handle()
-window.onpopstate = () => handle()
-window.route = () => route()
+// handle()
+// window.onpopstate = () => handle()
+// window.route = () => route()
 
 // a.addEventListener('click', function(event){})
